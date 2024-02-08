a8f4ab1c4ce560c52e5b72cbeb7e4c9258368c86
EugeneSarkisov <154362125+EugeneSarkisov@users.noreply.github.com>
2024-02-08 12:03:07 +0300

    Update Utils.java

--- src/com/teachmeskills/lesson9/task2/utils/Utils.java
+++ src/com/teachmeskills/lesson9/task2/utils/Utils.java
@@ -1,2 +1,16 @@
-package com.teachmeskills.lesson9.task2.utils;public class Utils {
+package com.teachmeskills.lesson9.task2.utils;
+
+/**
+ * This class contain method "arrOut" for printing matrix.
+ */
+
+public class Utils {
+    public static void arrOut(int[][] arr){
+        for (int[] arrays : arr){
+            for (int data : arrays){
+                System.out.print(data + " ");
+            }
+            System.out.println();
+        }
+    }
 }
